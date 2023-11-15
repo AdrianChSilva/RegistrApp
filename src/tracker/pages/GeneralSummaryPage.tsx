f1841fecda651d97fc4918821c8b4b81925bf4ff
Adrián <adrian.chamorrosilva@ottogroup.com>
2023-11-15 08:14:19 +0100

    añadido ejemplo de tracking de seguimiento

--- src/tracker/pages/GeneralSummaryPage.tsx
+++ src/tracker/pages/GeneralSummaryPage.tsx
@@ -1,5 +1,21 @@
+import React from "react";
 import flowers from "../../assets/images/hermosa-naturaleza-retro-flores.jpg";
+import { ProgressBar } from "primereact/progressbar";
 export const GeneralSummaryPage = () => {
+  const valueTemplate40 = (value: string) => {
+    return (
+      <React.Fragment>
+        {value}/<b>100</b>
+      </React.Fragment>
+    );
+  };
+  const valueTemplate90 = (value: string) => {
+    return (
+      <React.Fragment>
+        {value}/<b>100</b>
+      </React.Fragment>
+    );
+  };
   return (
     <div className="App">
       <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mb-3">
@@ -10,7 +26,7 @@ export const GeneralSummaryPage = () => {
               src={flowers}
             ></img>
           </div>
-          <div className="p-8">
+          <div className="p-8 grow" style={{ flexGrow: "1" }}>
             <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
               Curso React
             </div>
@@ -21,6 +37,12 @@ export const GeneralSummaryPage = () => {
               Alumno Apellido Apellido
             </a>
             <p className="mt-2 text-slate-500">Última vez actualizado: 1 día</p>
+            <div className="mt-8">
+              <ProgressBar
+                value={40}
+                displayValueTemplate={valueTemplate40}
+              ></ProgressBar>
+            </div>
           </div>
         </div>
       </div>
@@ -32,7 +54,7 @@ export const GeneralSummaryPage = () => {
               src={flowers}
             ></img>
           </div>
-          <div className="p-8">
+          <div className="p-8" style={{ flexGrow: "1" }}>
             <a
               href="#"
               className="uppercase tracking-wide text-sm text-indigo-500 font-semibold  hover:underline"
@@ -45,6 +67,12 @@ export const GeneralSummaryPage = () => {
             <p className="mt-2 text-slate-500">
               Última vez actualizado: 3 días
             </p>
+            <div className="mt-8">
+              <ProgressBar
+                value={90}
+                displayValueTemplate={valueTemplate90}
+              ></ProgressBar>
+            </div>
           </div>
         </div>
       </div>
